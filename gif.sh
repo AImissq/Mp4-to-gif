b45890671c818f6cd0332f88ba0a4cd832d94bee
AckroydAI <36539461+AImissq@users.noreply.github.com>
2025-06-29 20:58:56 +0100

    Update gif.sh

--- gif.sh
+++ gif.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 INPUT=$(zenity --file-selection --title="Select MP4 Video")
-if [ -z "$INPUT" ]; then exit; fi
-
+if [ -z "$INPUT" ]
+then
+    return $1
+  fi
+  
 OUTPUT=$(zenity --entry --title="Output GIF" --text="Enter output filename (e.g., output.gif):")
-if [ -z "$OUTPUT" ]; then exit; fi
-
+if [ -z "$OUTPUT" ]
+then
+    return $1
+  fi
+  
 ffmpeg -i "$INPUT" "$OUTPUT"
 
 zenity --info --text="Conversion complete!"
+exit
